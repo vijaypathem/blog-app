10fedcdc69f64fb07040bcef968290f061bbe437
Vijay Pathem <vijaypathem4@gmail.com>
2023-05-07 16:19:14 +0530

    user registration

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+# Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
